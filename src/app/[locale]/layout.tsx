795f449e8f1bbeb80b4c925af06de55cc2f77d40
Henning Sieh <admin@henningsieh.de>
2026-01-21 06:43:59 +0100

    fix(favicon): remove unused favicon links and add favicon.ico

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -76,24 +76,6 @@ export default async function LocaleLayout({ children, params }: Props) {
           href="https://fonts.gstatic.com"
           rel="preconnect"
         />
-        <link
-          href="/favicon/apple-touch-icon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="/favicon/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="/favicon/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link href="/favicon/site.webmanifest" rel="manifest" />
         {env.NODE_ENV === "development" && (
           <script
             async
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon/favicon.ico", sizes: "any" },
       { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
